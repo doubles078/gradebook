6d6fc7ae5ec13316597b6bc0184a02027ac3d47d
Dan Donohue <dan.donohue@EF.com>
2020-03-06 12:37:56 -0500

    feat: first commit

---
+++ src/Gradebook/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Gradebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var grades = new List<double>() { 81.2, 98.2, 87.2, 98.4 };
+            double sum = 0.0;
+
+
+            foreach (double number in grades)
+            {
+                sum += number;
+            }
+
+            double average = sum / grades.Count;
+
+            System.Console.WriteLine($"The average grade is {average:N1}.");
+            System.Console.WriteLine(sum);
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine($"Hello baby cakes!");
+            }
+        }
+    }
+}
